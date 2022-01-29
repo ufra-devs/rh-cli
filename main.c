c8bd5874bedf96e1a8875c00c328b5d9cd789e8a
Atiladanvi <atila.danvi@outlook.com>
2022-01-29 10:26:35 -0300

    Adiciona função buscar funcionário e melhora interação com o usuário

--- main.c
+++ main.c
@@ -3,13 +3,18 @@
 #include <stdlib.h>
 #include <unistd.h>
 
-void adicionar_funcionario(); // átila
-void listar_funcionarios();   // marília
-void atualizar_funcionario(); // irinaldo;
-void excluir_funcionario();   // marília
-int last_id();                // átila/marília/irinaldo
-int increment_id(int id);     // átila/marília/irinaldo
+/* Funções do programa */
+void adicionar_funcionario();// átila
+char *buscar_funcionario();// átila
+void listar_funcionarios();// marília
+void atualizar_funcionario();// irinaldo;
+void excluir_funcionario();// marília
 
+/* Funções do sistema */
+int last_id();// átila/marília/irinaldo
+int increment_id(int id);// átila/marília/irinaldo
+
+/* Constantes */
 #define BUFFER_SIZE 1000
 #define DATABASE_FILE "database.txt"
 #define FUNCIONARIOS_FILE "funcionarios.txt"
@@ -19,30 +24,39 @@ int main()
     char data[BUFFER_SIZE];
     int acao;
 
-    do
-    {
+    do {
         system("clear");
-        printf("\n\nRH-cli\n\n");
+        printf("RH-cli\n");
+        printf("-------------------------\n");
         printf("1 - Cadastrar Funcionário\n");
+        printf("2 - Buscar Funcionário\n");
         printf("0 - Sair\n");
+        printf("-------------------------\n");
         scanf("%d", &acao);
 
         switch (acao)
         {
             case 0:
-                printf("\nSaindo...\n");
-                sleep(2);
                 system("clear");
                 return 0;
             case 1:
+                system("clear");
                 fgets(data, BUFFER_SIZE, stdin);
                 adicionar_funcionario();
                 break;
+            case 2:
+                system("clear");
+                fgets(data, BUFFER_SIZE, stdin);
+                buscar_funcionario();
+                break;
             default:
+                system("clear");
                 printf("Opção inválida\n");
+                printf("--------------\n");
+                printf("Voltando...\n");
+                sleep(1);
         }
-
-    } while (acao != acao + 1);
+    } while (acao != acao + 10);
 
     return 0;
 }
@@ -55,7 +69,7 @@ void adicionar_funcionario()
     char email[BUFFER_SIZE];
     char data_de_admissao[BUFFER_SIZE];
     char salario[BUFFER_SIZE];
-    FILE *file;
+    FILE * file;
 
     int new_id;
     char id[BUFFER_SIZE];
@@ -63,6 +77,8 @@ void adicionar_funcionario()
     new_id = last_id() + 1;
     sprintf(id, "%d", new_id);
 
+    printf("Castrando funcionário\n");
+    printf("---------------------\n\n");
     printf("Digite: Nome\n");
     fgets(nome, BUFFER_SIZE, stdin);
     nome[strcspn(nome, "\n")] = 0;
@@ -95,13 +111,103 @@ void adicionar_funcionario()
     system("clear");
     printf("\nVoltando para o menu principal...\n");
     sleep(1);
+}
 
-    return;
+char *buscar_funcionario()
+{
+    char *line_buf = NULL;
+    size_t line_buf_size = 0;
+    int line_count = 0;
+    ssize_t line_size;
+    FILE *file = fopen(FUNCIONARIOS_FILE, "r");
+    char id[BUFFER_SIZE];
+    char *record_id = NULL;
+    char record[BUFFER_SIZE];
+    int founded = 0;
+    char header[6][20] = {
+            { "ID" },
+            { "MATRÍCULA" },
+            { "NOME" },
+            { "EMAIL" },
+            { "DATA DE ADMISSÃO" },
+            { "SALÁRIO" }
+    };
+
+    /* obtém todas as linhas do arquivo */
+    line_size = getline(&line_buf, &line_buf_size, file);
+
+    printf("Buscando funcionário\n");
+    printf("--------------------\n\n");
+    printf("Digite: id\n");
+    fgets(id, BUFFER_SIZE, stdin);
+    /* remove caracteres inválidos digitado pelo usuário */
+    id[strlen(id) - 1] = 0;
+
+    /* percorre as linhas do arquivo */
+    while (line_size >= 0)
+    {
+        /* incrementada o contador de linhas */
+        line_count++;
+
+        /* copia o valor da linha para o registro */
+        strcpy(record, line_buf);
+
+        /* obtém o id do resgistro corrente */
+        record_id = strtok(line_buf, ";");
+
+        /* verifica se o id fornecido é igual ao item corrente do loop */
+        if (strcmp(id, record_id) == 0)
+        {
+            founded = 1;
+            system("clear");
+            printf("Funcionário encontrado\n");
+            printf("------------------------\n\n");
+
+            /* Obtém os campos registro */
+            char *token = strtok(record, ";");
+
+            /* Faz a impresão dos campos do funcionário encontrado */
+            int i = 0;
+            while (token != NULL)
+            {
+                printf("%s: %s\n", header[i], token);
+                token = strtok(NULL, ";");
+                i++;
+            }
+
+            printf("------------------------\n\n");
+            /* interrompe o bloco de repetição se o registro porque o foi encontrado */
+            break;
+        }
+
+        /* obtém a próxima linha */
+        line_size = getline(&line_buf, &line_buf_size, file);
+    }
+
+    /* libera buffer */
+    free(line_buf);
+    line_buf = NULL;
+
+    /* fecha arquivo */
+    fclose(file);
+
+    /* verifica se o registro não foi encontrado */
+    if (founded == 0)
+    {
+        system("clear");
+        printf("\n404 - Funcionário não encontrado\n");
+        printf("---------------------------------\n\n");
+    }
+
+    printf("Enter para voltar\n");
+    fgets(id, BUFFER_SIZE, stdin);
+
+    return EXIT_SUCCESS;
 }
 
 int last_id()
 {
-    FILE *file;
+    FILE * file;
     int id;
     char id_str[BUFFER_SIZE];
 
@@ -120,7 +226,7 @@ int last_id()
 
 int increment_id(int id)
 {
-    FILE *file;
+    FILE * file;
     char id_str[BUFFER_SIZE];
 
     sprintf(id_str, "%d", id);
